81b572fc80ac099b3a11bcfbda768d292de7c63f
Fitz Ignacio <ignacio.fitz@gmail.com>
2023-01-22 20:11:36 -0800

    commit tree a7bacab18d2c4f1be5a2983882bab6190ab9ba65

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left);
+    if (left > 0) {
+      left -= 5;
+      dodger.style.left = `${left}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    let left = parseInt(dodger.style.left);
+    left += 5;
+    dodger.style.left = `${left}px`;
+  }
